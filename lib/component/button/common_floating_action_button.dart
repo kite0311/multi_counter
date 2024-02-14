5f26b2a805709202d168746f472650a678f3dce1
Kaito Kannari <19970311kk@gmail.com>
2024-02-15 03:19:45 +0900

    update: SizedBox内の値を修正

--- lib/component/button/common_floating_action_button.dart
+++ lib/component/button/common_floating_action_button.dart
@@ -11,11 +11,11 @@ class CommonFloatingActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 100,
-      width: 100,
+      height: 78,
+      width: 78,
       child: FloatingActionButton(
         shape: CircleBorder(),
-        backgroundColor: Theme.of(context).primaryColorDark,
+        backgroundColor: Colors.blue,
         onPressed: onPressed,
         child: const Icon(Icons.add_circle_outline),
       ),
