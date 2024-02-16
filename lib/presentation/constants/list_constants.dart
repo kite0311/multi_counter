54440b1a088ad2d2154be3dc7935125c65ee495d
Kaito Kannari <19970311kk@gmail.com>
2024-02-17 02:51:14 +0900

    add: 定数の追加

---
+++ lib/presentation/constants/list_constants.dart
@@ -0,0 +1,4 @@
+class ListConstantsValue {
+  // リストの初期化値
+  static const int listInitializationValue = 5;
+}
--- lib/presentation/theme/size.dart
+++ lib/presentation/theme/size.dart
@@ -1,1 +1,18 @@
-class CommonSize {}
+class CommonSize {
+  /// c = container
+  static const c0 = 0.0;
+  static const c50 = 50.0;
+  static const c100 = 100.0;
+  static const c200 = 200.0;
+
+  // f = fontSize
+  static const f12 = 12.0;
+  static const f16 = 16.0;
+  static const f20 = 20.0;
+
+  /// p = padding
+  static const p0 = 0.0;
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p16 = 16.0;
+}
