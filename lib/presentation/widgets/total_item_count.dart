eddadea16f33d189d84eaac4321e4c25b7f9d95a
Kaito Kannari <19970311kk@gmail.com>
2024-02-17 02:55:04 +0900

    add: 合計値表示用カスタムウィジェットの実装

---
+++ lib/presentation/widgets/total_item_count.dart
@@ -0,0 +1,24 @@
+import 'package:counter/presentation/theme/size.dart';
+import 'package:flutter/material.dart';
+
+class TotalItemCountWidget extends StatelessWidget {
+  const TotalItemCountWidget({super.key, required this.totalItemCount});
+
+  final int totalItemCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: CommonSize.p16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Total: Items: $totalItemCount',
+            style: const TextStyle(fontSize: CommonSize.f16),
+          ),
+        ],
+      ),
+    );
+  }
+}
