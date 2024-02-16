7b3778d580e8d2eb15b020678f37b9ecc79fd800
Kaito Kannari <19970311kk@gmail.com>
2024-02-17 02:55:57 +0900

    update: リストの初期化処理の追加とappbarにカスタムウィジェットを設定

--- lib/presentation/pages/card_list.dart
+++ lib/presentation/pages/card_list.dart
@@ -1,26 +1,39 @@
 import 'package:counter/presentation/widgets/buttons/add_list_button.dart';
 import 'package:counter/presentation/widgets/card_list/common_card_list.dart';
 import 'package:counter/presentation/widgets/common_bottom_appbar.dart';
+import 'package:counter/presentation/widgets/total_item_count.dart';
 import 'package:flutter/material.dart';
 
-
 class CardListPage extends StatefulWidget {
-  const CardListPage({super.key});
+  const CardListPage({super.key, required this.InitialItems});
+
+  final List<String> InitialItems;
 
   @override
-  State<CardListPage> createState() => _CardListScreen();
+  State<CardListPage> createState() => _CardListState();
 }
 
-class _CardListScreen extends State<CardListPage> {
-  int number = 0;
-  List<String> itemList = [];
+class _CardListState extends State<CardListPage> {
+  // itemListをlate修飾子で初期化
+  late List<String> itemList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // 初期化時に渡された値をitemListに代入し初期化
+    itemList = List<String>.from(widget.InitialItems);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('multi_counter'),
+        title: const Text('Multi Counter App'),
+        actions: [
+          // 合計値を表示するウィジェット
+          TotalItemCountWidget(totalItemCount: itemList.length),
+        ],
       ),
       body: Center(
         child: Column(
