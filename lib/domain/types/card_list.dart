282a0543c956f2db249955975b03610d7d50c4ba
Kaito Kannari <19970311kk@gmail.com>
2024-02-23 03:11:04 +0900

    add: CardList型のクラスを追加

---
+++ lib/domain/types/card_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardList {
+  String id;
+  String listname;
+  Color listcolor;
+  int listSettingVal;
+
+  CardList({
+    required this.id,
+    required this.listname,
+    required this.listcolor,
+    required this.listSettingVal,
+  });
+}
