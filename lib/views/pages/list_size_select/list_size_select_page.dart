34f59077481c272a46d005b22e45423c337cda3c
Kaito Kannari <19970311kk@gmail.com>
2024-03-11 02:51:11 +0900

    add: add new page

---
+++ lib/views/pages/list_size_select/list_size_select_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListSizeSelectPage extends StatelessWidget {
+  const ListSizeSelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
